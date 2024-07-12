739ce5bd43109e1a73b13c60000c65a4f6514d1e
Janaina Pires <dev.janapiress@gmail.com.com>
2024-07-12 10:57:05 -0300

    Estou adicionando este arquivo somente para registrar como adicionar novos arquivos no git

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,4 @@
 print("hello world, my name is Jana!")
 print("Hoje está nublado")
 print("Mas pelo menos não está chovendo")
+print("Demonstração do Github")
---
+++ novoteste.py
@@ -0,0 +1,4 @@
+# testando operações do github e fazendro commit e pushes para salvar.
+# Que coisa chata!!!
+
+print("Demonstrando essas alterações")
