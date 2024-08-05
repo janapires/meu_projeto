82bb76ba2bc15aa94db58db9bb53fbf22d9f1a47
Janaina Pires <dev.janapiress@gmail.com>
2024-08-05 11:49:16 -0300

    Falta resolver o problema da tabuada

---
+++ estruturasderepeticao_tarefa1_tabuada.py
@@ -0,0 +1,17 @@
+# Criando um programa pra uma tabuada de muktiplicação
+
+numero = int(input())
+contador = numero * 7
+print("Digite o número que será multiplicado: ")
+
+# Montando a estrutura da tabuada
+X = 0
+print("Digite o número: ", numero)
+while numero:
+    print(contador)
+    X = X * contador
+    
+
+
+
+
