c3a3925b96d9372397bc2063336f5725c5358aba
Janaina Pires <dev.janapiress@gmail.com>
2024-08-05 10:13:19 -0300

    Testando o continue

--- estrurutaderepeticao_usando_continue.py
+++ estrurutaderepeticao_usando_continue.py
@@ -4,7 +4,7 @@ numero = 1
 while numero >= 0:
     print("Digite um número negativo para sair: ")
     numero = int(input())
-    break
+    continue
     print("Este texto não será exibido! Mas...")
 else:
     print("O número digitado foi: ", numero)
