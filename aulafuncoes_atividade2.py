03b5d64f09f0b5097d345513084646e9cf570289
Janaina Pires <dev.janapiress@gmail.com>
2024-08-07 10:40:05 -0300

    Definindo operações completas em funções

---
+++ aulafuncoes_atividade2.py
@@ -0,0 +1,26 @@
+# Nova demonstração do uso de funções...
+
+# Definindo o bloco de instruções!
+
+def adicao(x, y):
+    return x + y
+def subtracao(x, y):
+    return x - y
+
+# Inserindo dados para determinar a operação:
+
+print("Digite dois valores de números inteiro: ")
+n1 = int(input("x: "))
+n2 = int(input("y: "))
+op = input("Qual operação deseja realizar? (+ ou -)")
+
+# Definindo o resultado da intrução:
+
+if op == "+":
+    z = adicao(n1, n2)
+    print("O resultado da soma é: ", z)
+elif op == "-":
+    z = subtracao(n1, n2)
+    print("O resultado da subtração é: ", z)
+else:
+    print("Operação digitada inexistente!")
