b96ed102b00bec9d6935d001c40f11b797252d20
Janaina Pires <dev.janapiress@gmail.com>
2024-08-12 11:52:04 -0300

    Tarefa para realizar em casa!!!!

---
+++ tarefa3_aula_listas_criando_lista_de_tarefas.py
@@ -0,0 +1,17 @@
+# Tarefa da aula de lista, métodos e funções
+
+# Declarando a lista de tarefas... 
+
+print("Eis a minha lista de tarefas do dia: ")
+listaTarefas = ["1. Arrumar casa",
+                "2. Ir às compras",
+                "3. Fazer almoço",
+                "4. Passear com o cachorro",
+                "5. Tirar o lixo"]
+
+# Definindo a execução das tarefas
+
+print(" Você já realizou a sua primeira tarefa hoje? (S/N)")
+
+# terminar em casa
+
